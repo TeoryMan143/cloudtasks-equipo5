9a25c69aabf39518f06812050d37ba26d770ef4e
tarooanna <anabejarano1970@gmail.com>
2026-09-04 14:46:00 -0500

    feat: Add edit task functionality and update dependencies

--- src/schemas.ts
+++ src/schemas.ts
@@ -8,3 +8,12 @@ export const createTaskSchema = z.object({
   }),
   priority: z.enum(['low', 'mid', 'high']),
 });
+
+export const editTaskSchema = z.object({
+  title: z.string().min(2, 'Add a title'),
+  description: z.string().min(2, 'Add a description'),
+  deadline: z.date().refine((date) => date > new Date(), {
+    message: 'Deadline must be in the future',
+  }),
+  priority: z.enum(['low', 'mid', 'high']),
+});
